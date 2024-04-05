0c89ceb206395abd6763e573c4ce8974b9393b05
Gonzalo Hidalgo Madrid <gonzalo@powerlinedesign.es>
2024-04-05 04:44:49 +0200

    :sparkles: Añadido de visualizacion de contraseña + texto informativo + mostrar logotipo

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,7 +1,9 @@
 import React, { useState, useContext } from 'react';
-import { View, Text, TextInput, TouchableOpacity, Image, StyleSheet, Alert } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity, Image, Alert, ScrollView } from 'react-native';
 import styles from './Styles/LoginScreenStyles';
 import { AuthContext } from '../context/AuthContext'; // Asumiendo que tienes un AuthContext
+import {Icon} from '@rneui/themed';
+
 
 const LoginScreen = ({ navigation }) => {
   const [username, setUsername] = useState('');
@@ -19,33 +21,59 @@ const LoginScreen = ({ navigation }) => {
     }
   };
 
+  const [showPassword, setShowPassword] = useState(false);
+
   return (
     <View style={styles.container}>
-      <Text style={styles.welcomeBack}>Hola, ¡Bienvenido de nuevo! 👋</Text>
-      <Text style={styles.introText}>Empiece a gestionar su negocio de Magento con nosotros</Text>
-      <TextInput
-        value={username}
-        onChangeText={setUsername}
-        placeholder="Usuario"
-        style={styles.input}
-        keyboardType="default"
-        autoCapitalize="none"
-      />
-      <TextInput
-        value={password}
-        onChangeText={setPassword}
-        placeholder="Contraseña"
-        style={styles.input}
-        secureTextEntry
-      />
-      <TouchableOpacity onPress={() => {/* Navegar a la pantalla de recuperación de contraseña */}}>
-        <Text style={styles.forgotPasswordText}>¿Ha olvidado su contraseña?</Text>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={handleLogin} style={styles.loginButton}>
-        <Text style={styles.loginButtonText}>Iniciar Sesión</Text>
-      </TouchableOpacity>
-    </View>
-  );
-};
+      <ScrollView style={styles.scrollView}>
+            {/* <Image source={require('../assets/logo.png')} style={{width: 380, height: 120, alignSelf: 'center'}} /> */}
+            <Text style={styles.welcomeBack}>Hola, ¡Bienvenido de nuevo! 👋</Text>
+            <Text style={styles.introText}>Empiece a gestionar su negocio de Magento con nosotros</Text>
+            <TextInput
+              value={username}
+              onChangeText={setUsername}
+              placeholder="Usuario"
+              style={styles.inputUser}
+              keyboardType="default"
+              autoCapitalize="none"
+              autoFocus
+              placeholderTextColor={'#999'}
+              selectionColor={'#FFA000'} // Color del cursor
+            />
+            <View style={styles.inputContainer}>
+            <TextInput
+              value={password}
+              onChangeText={setPassword}
+              placeholder="Contraseña"
+              style={styles.input}
+              secureTextEntry={!showPassword}
+              placeholderTextColor={'#999'} 
+              selectionColor={'#FFA000'} // Color del cursor          
+            /> 
+            <TouchableOpacity
+            style={styles.toggleShowPassword}
+            onPress={() => setShowPassword(!showPassword)}
+          >
+            <Icon 
+              name={showPassword ? 'eye' : 'eye-slash'} // Cambia según el estado de showPassword
+              type='font-awesome' // Asegúrate de tener esta línea si usas @rneui/themed
+              color={showPassword ? '#000' : '#999'}               
+              size={24}
+            />
+          </TouchableOpacity>
+          </View>   
+            <TouchableOpacity onPress={() => {/* Navegar a la pantalla de recuperación de contraseña */}} >
+              <Text style={styles.forgotPasswordText}>¿Ha olvidado su contraseña?</Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={handleLogin} style={styles.loginButton}>
+              <Text style={styles.loginButtonText}>Iniciar Sesión</Text>
+            </TouchableOpacity>
+            <Text style={styles.finalText}>
+              <Text style={{ fontWeight: 'bold' }}>Magento Mobile Admin</Text> es una plataforma de gestión open source para Magento. Adaptable y personalizable, lo que ves es solo el inicio. Únete, explora y adapta esta demo a tus necesidades de negocio.
+            </Text>
+            </ScrollView>
+          </View>
+        );
+      };
 
 export default LoginScreen;
--- src/screens/Styles/LoginScreenStyles.js
+++ src/screens/Styles/LoginScreenStyles.js
@@ -16,9 +16,19 @@ export default StyleSheet.create({
   introText: {
     fontSize: 16,
     color: '#666', // Este es un color gris, ajusta según tu diseño
-    marginBottom: 40,
+    marginBottom: 20,
   },
-  input: {
+  inputContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    borderColor: '#ddd',
+    borderWidth: 1,
+    borderRadius: 10,
+    paddingHorizontal: 15,
+    marginBottom: 15,
+  },
+  inputUser: {
+    flex: 1,
     height: 50,
     borderColor: '#ddd', // Color de borde del input
     borderWidth: 1,
@@ -26,6 +36,19 @@ export default StyleSheet.create({
     paddingHorizontal: 15,
     fontSize: 16,
     marginBottom: 15,
+    color: '#444', // Color del texto del input
+  },
+  input: {
+    flex: 1,
+    height: 50,
+    fontSize: 16,
+    color: '#444', // Mantén el color que ya tenías
+  },
+  toggleShowPassword: {
+    marginLeft: 10,
+  },
+  icon: {
+    color: '#999',
   },
   forgotPasswordText: {
     color: '#FFA000', // Color del enlace Olvidé mi contraseña
@@ -50,4 +73,35 @@ export default StyleSheet.create({
     fontSize: 18,
     fontWeight: 'bold',
   },
-});
+  showPasswordButton: {
+    backgroundColor: '#FFA000', // Color de fondo del botón de mostrar contraseña
+    borderRadius: 10,
+    height: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    elevation: 3, // Sombra en Android
+    // Sombra para iOS (opcional)
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.3,
+    marginBottom: 15,
+    },
+    showPasswordButtonText: {
+    color: '#fff',
+    fontSize: 18,
+    fontWeight: 'bold',
+    },
+    logo: {
+    alignSelf: 'center',
+    width: '100%',
+    aspectRatio: 1, // Maintain aspect ratio of the SVG
+    marginBottom: 15,
+    },
+    finalText: {
+      fontSize: 16,
+      color: '#666', // Este es un color gris, ajusta según tu diseño
+      marginTop: 20,
+      textAlign: 'center',
+    },
+  });
