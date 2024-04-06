de0db2535c5f2b098ff62b64aca59048cff4ebfd
Gonzalo Hidalgo Madrid <gonzagamer79@gmail.com>
2024-04-06 02:46:22 +0200

    :hammer: Implementacion de función para cerrar sesion desde el menu

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,5 +1,6 @@
 import React, { createContext, useState } from 'react';
 import { adminLogin } from '../utils/auth'; // Tu función de autenticación
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export const AuthContext = createContext({});
 
@@ -10,6 +11,7 @@ export const AuthProvider = ({ children }) => {
     try {
       const token = await adminLogin(username, password);
       setUserToken(token); // Almacena el token en el estado
+      await AsyncStorage.setItem('userToken', token);
       // También podrías querer guardar el token en AsyncStorage o en un lugar seguro aquí
     } catch (error) {
       // Manejar el error, por ejemplo mostrar un mensaje al usuario
@@ -17,8 +19,17 @@ export const AuthProvider = ({ children }) => {
     }
   };
 
+  const signOut = async () => {
+    try {
+      await AsyncStorage.removeItem('userToken'); // Asegúrate de limpiar el token de AsyncStorage
+      setUserToken(null); // Resetea el estado del token a null
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
-    <AuthContext.Provider value={{ userToken, signIn }}>
+    <AuthContext.Provider value={{ userToken, signIn, signOut }}>
       {children}
     </AuthContext.Provider>
   );
